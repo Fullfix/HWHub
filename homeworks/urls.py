bc456b5a9b64443593ca6d3626d32c09112e6e6e
glyway <55627397+glyway@users.noreply.github.com>
2019-10-01 16:56:19 +0400

    Smol fix

--- homeworks/urls.py
+++ homeworks/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('', views.books, name='books'),
-	path('algebra/<book>/<int:p>/<int:num>', views.get_hw, name='algebra'),
+	path('algebra/<str:book>/<int:p>/<int:num>', views.get_hw, name='algebra'),
 	path('algebra/upload', views.upload_hw, name='upload'),
 ]
