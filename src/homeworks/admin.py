383e58596fb9bf749a74bae809dd8fe8bb449486
Nikita <nbreskanu73@gmail.com>
2020-04-16 16:21:15 +0400

    customized admin site

--- src/homeworks/admin.py
+++ src/homeworks/admin.py
@@ -3,9 +3,42 @@ from .models import Homework, HomeworkImage, Grade, Subject, Book, New
 
 # Register your models here.
 
-admin.site.register(Homework)
-admin.site.register(HomeworkImage)
-admin.site.register(Grade)
-admin.site.register(Subject)
-admin.site.register(Book)
-admin.site.register(New)
+@admin.register(Homework)
+class HomeworkAdmin(admin.ModelAdmin):
+	list_display = ('publisher', 'grade', 'subject',
+		'book', 'number', 'publication_date', 'like_count', 'dislike_count')
+	list_display_links = ('number', 'publisher')
+	list_filter = ('publication_date',)
+
+	def like_count(self, obj):
+		return str(obj.likes.count())
+
+	def dislike_count(self, obj):
+		return str(obj.likes.count())
+
+@admin.register(HomeworkImage)
+class HomeworkImageAdmin(admin.ModelAdmin):
+	list_display = ('homework', 'index', 'image')
+
+@admin.register(Grade)
+class GradeAdmin(admin.ModelAdmin):
+	list_display = ('grade',)
+
+@admin.register(Subject)
+class SubjectAdmin(admin.ModelAdmin):
+	list_display = ('grade', 'name', 'full_name')
+	list_display_links = ('grade', 'name')
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+	list_display = ('name', 'full_name',
+		'slug', 'subject', 'grade', 'image', 'types')
+	list_display_links = ('name', 'full_name', 'slug')
+
+	def grade(self, obj):
+		return str(obj.subject.grade.grade)
+
+@admin.register(New)
+class NewAdmin(admin.ModelAdmin):
+	list_display = ('publisher', 'summary', 'description', 'publication_date')
+	list_filter = ('publication_date',)
--- src/users/admin.py
+++ src/users/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from .models import UserProfile, User
 
-admin.site.register(User)
-admin.site.register(UserProfile)
+
+class UserAdmin(admin.ModelAdmin):
+	list_display = ('username', 'admin', 'staff')
+
+class UserProfileAdmin(admin.ModelAdmin):
+	list_display = ('user', 'name', 'surname', 'photo')
+
+
+admin.site.unregister(Group)
+admin.site.register(User, UserAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
